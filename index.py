ae1bbf8537e9c46ae7a925e54980f7044ab19b18
kosobokova_olya <kosobokova.olga.i@gmail.com>
2026-09-12 20:28:38 +0300

    Update index.py

--- index.py
+++ index.py
@@ -3,4 +3,13 @@ print('Hello world')
 for i in range(5):
     print(i)
 
+<<<<<<< Updated upstream
 ptint('Hi')
+=======
+ptint('Hi')
+print('it is zhopa')
+print('<3')
+
+for i in range(5):
+    print(i)
+>>>>>>> Stashed changes
